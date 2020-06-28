a08869c1eb260bc5822e6a0203b83a90b0a6363e
Joseph Hilber <joeyhilber@gmail.com>
2020-06-28 16:28:35 -0400

    Update with basic files.

---
+++ lib/data/music.dart
@@ -0,0 +1,69 @@
+class Music {
+  final String title, author, album, label, description, authorImg, albumImg, authorC, imageC;
+  final DateTime release;
+  final List<String> genre, writer, producer;
+
+  Music(
+    this.title,
+    this.author,
+    this.authorImg,
+    this.authorC,
+    this.album,
+    this.albumImg,
+    this.imageC,
+    this.release,
+    this.label,
+    this.description,
+    this.genre,
+    [this.writer,
+    this.producer]
+  );
+
+}
+
+List<Music> songs = [
+  //Prom Night: 0
+  Music(
+      "Prom Night",
+      "Chance the Rapper",
+      "chance.png",
+      "FOX Sports: https://www.youtube.com/watch?v=YT86shZnD38, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=72191644",
+      "10 Day",
+      "10DayTape.jpeg",
+      "Fair use: https://en.wikipedia.org/w/index.php?curid=50047311",
+      DateTime(2012, 4, 3),
+      "Independent",
+      "desc to be completed later",
+      ["Hip-Hop", "R&B", "Rap"]
+  ),
+  //I Wanna Get Better: 1
+  Music(
+      "I Wanna Get Better",
+      "Chance the Rapper",
+      "chance.png",
+      "FOX Sports: https://www.youtube.com/watch?v=YT86shZnD38, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=72191644",
+      "10 Day",
+      "10DayTape.jpeg",
+      "Fair use: https://en.wikipedia.org/w/index.php?curid=50047311",
+      DateTime(2012, 4, 3),
+      "Independent",
+      "desc to be completed later",
+      ["Hip-Hop", "R&B", "Rap"]
+  ),
+  //Crash My Car
+  Music(
+      "Crash My Car",
+      "Chance the Rapper",
+      "chance.png",
+      "FOX Sports: https://www.youtube.com/watch?v=YT86shZnD38, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=72191644",
+      "10 Day",
+      "10DayTape.jpeg",
+      "Fair use: https://en.wikipedia.org/w/index.php?curid=50047311",
+      DateTime(2012, 4, 3),
+      "Independent",
+      "desc to be completed later",
+      ["Hip-Hop", "R&B", "Rap"]
+  ),
+
+
+];
---
+++ lib/defaults.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const secondaryColor = Color(0xff65def1);
+const thirdColor = Color(0xfff17f29);
+const textColor = Color(0xff121212);
+const textLightColor = Color(0xffa1a1a1);
+
+const defaultPadding = 16.0;
+
+const defaultShadow = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: textColor,
+);
