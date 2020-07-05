afc9e1a75bd7ba333b662eeb62c4f35c3bab9af9
Joseph Hilber <joeyhilber@gmail.com>
2020-07-05 10:48:17 -0400

    Add the layout back for the main body of each category after testing navigation.

--- lib/screens/home/components/songs.dart
+++ lib/screens/home/components/songs.dart
@@ -19,12 +19,16 @@ class _SongList extends State<SongList> {
   final scroll;
   final Function updateUnderline;
   _SongList(this.scroll, this.updateUnderline);
+  ScrollController _controller;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: 500,
+      height: MediaQuery.of(context).size.width -
+          (MediaQuery.of(context).padding.top -
+              MediaQuery.of(context).padding.bottom -
+              132.0),
         child: PageView.builder(
           onPageChanged: (i) {
             setState(() {
@@ -35,15 +39,158 @@ class _SongList extends State<SongList> {
           scrollDirection: Axis.horizontal,
           itemCount: categories.length,
           itemBuilder: (context, i) {
-            return Container(
-              width: 30,
-              height: 80,
-              color: Colors.grey,
-              child: Center(
-                child: Text(
-                  "${i}",
+            return Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 12.0),
                 ),
-              ),
+                Expanded(
+//                  width: MediaQuery.of(context).size.width,
+//                  height: MediaQuery.of(context).size.width -
+//                      (MediaQuery.of(context).padding.top -
+//                          MediaQuery.of(context).padding.bottom -
+//                          132.0),
+                  child: ListView.builder(
+                    controller: _controller,
+                    scrollDirection: Axis.vertical,
+                    itemCount: 1,
+                    itemBuilder: (context, temp) {
+                      List<Music> current = getAtIndex(i);
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: defaultPadding, vertical: defaultPadding),
+                        child: Column(
+                          children: <Widget>[
+                            //New Picks section-----------------------------------------
+                            Container(
+                              height: 260.0,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.transparent,
+                              child: Container(
+                                padding: EdgeInsets.all(defaultPadding),
+                                decoration: defaultDecoration,
+                                child: Column(
+                                  children: <Widget>[
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        "New Picks",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding: EdgeInsets.symmetric(vertical: 8.0)),
+                                    Expanded(
+//                                  padding: EdgeInsets.symmetric(vertical: defaultPadding),
+//                                  height: 200,
+//                                  width: MediaQuery.of(context).size.width,
+//                                  color: c,
+                                      child: Container(
+                                        height: double.infinity,
+                                        width: MediaQuery.of(context).size.width,
+                                        //TODO in this container, show the latest 5 songs
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.symmetric(vertical: defaultPadding * 1.5)),
+                            //Featured section------------------------------------------
+                            Container(
+                              //height: 520.0,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.transparent,
+                              child: Container(
+                                padding: EdgeInsets.all(defaultPadding),
+                                decoration: defaultDecoration,
+                                child: Column(
+                                  children: <Widget>[
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        "Featured",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    ListView.builder(
+                                        shrinkWrap: true,
+                                        physics: NeverScrollableScrollPhysics(),
+                                        itemCount: featured(getAtIndex(i)).length,
+                                        itemBuilder: (context, j) {
+                                          Music current = getAtIndex(i)[j];
+                                          return Padding(
+                                            padding: EdgeInsets.only(top: 8.0),
+                                            child: Row(
+                                              children: <Widget>[
+                                                Image.network(current.getAlbumImg, width: 120, height: 120,),
+                                                Padding(padding: EdgeInsets.symmetric(horizontal: 8.0)),
+                                                Container(
+                                                  height: 120,
+                                                  child: Align(
+                                                    alignment: Alignment.topLeft,
+                                                    child: Column(
+                                                      children: <Widget>[
+                                                        Text(
+                                                          current.getTitle + "\n" + current.getArtist + "\n" + current.getAlbum,
+                                                          style: TextStyle(
+                                                            fontSize: 16,
+                                                            fontWeight: FontWeight.w500,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                        }
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.symmetric(vertical: defaultPadding * 1.5)),
+                            //Other section
+                            Container(
+                              height: 1040.0,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.transparent,
+                              child: Container(
+                                padding: EdgeInsets.all(defaultPadding),
+                                decoration: defaultDecoration,
+                                child: Column(
+                                  children: <Widget>[
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        "Other Selections",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    //TODO add all other songs here
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             );
           },
         ),
