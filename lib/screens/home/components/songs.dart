0a7f0342179e20c6ad980c91043fcf22801f8589
Joseph Hilber <joeyhilber@gmail.com>
2020-07-09 10:59:58 -0400

    Modify layout and add "Other Selections" category

--- lib/screens/home/components/songs.dart
+++ lib/screens/home/components/songs.dart
@@ -67,9 +67,7 @@ class _SongList extends State<SongList> {
                               ),
                             ),
                           ),
-                          Padding(
-                              padding:
-                              EdgeInsets.symmetric(vertical: 8.0)),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
                           Container(
                             height: 260.0,
                             width: MediaQuery.of(context).size.width,
@@ -82,9 +80,13 @@ class _SongList extends State<SongList> {
                                   Container(
                                     child: Container(
                                       foregroundDecoration: new BoxDecoration(
-                                        color: Colors.purple,
                                         gradient: new LinearGradient(
-                                          colors: [Colors.white, Colors.white.withOpacity(0.0), Colors.white.withOpacity(0.0), Colors.white],
+                                          colors: [
+                                            Colors.white,
+                                            Colors.white.withOpacity(0.0),
+                                            Colors.white.withOpacity(0.0),
+                                            Colors.white
+                                          ],
                                           stops: [0, .05, .95, 1],
                                         ),
                                       ),
@@ -92,19 +94,19 @@ class _SongList extends State<SongList> {
                                       width: MediaQuery.of(context).size.width,
                                       child: CarouselSlider.builder(
                                         options: CarouselOptions(
-                                            aspectRatio: 16 / 9,
-                                            viewportFraction: 0.6,
-                                            initialPage: 0,
-                                            enableInfiniteScroll: true,
-                                            reverse: false,
-                                            autoPlay: true,
-                                            autoPlayInterval:
-                                                Duration(seconds: 5),
-                                            autoPlayAnimationDuration:
-                                                Duration(milliseconds: 1000),
-                                            autoPlayCurve: Curves.easeInOut,
-                                            enlargeCenterPage: true,
-                                            scrollDirection: Axis.horizontal,
+                                          aspectRatio: 16 / 9,
+                                          viewportFraction: 0.6,
+                                          initialPage: 0,
+                                          enableInfiniteScroll: true,
+                                          reverse: false,
+                                          autoPlay: true,
+                                          autoPlayInterval:
+                                              Duration(seconds: 5),
+                                          autoPlayAnimationDuration:
+                                              Duration(milliseconds: 1000),
+                                          autoPlayCurve: Curves.easeInOut,
+                                          enlargeCenterPage: true,
+                                          scrollDirection: Axis.horizontal,
                                         ),
                                         itemCount: 5,
                                         itemBuilder: (context, index) {
@@ -115,7 +117,8 @@ class _SongList extends State<SongList> {
                                                     Radius.circular(8.0)),
                                                 child: Stack(
                                                   children: <Widget>[
-                                                    Image.network("https://upload.wikimedia.org/wikipedia/en/1/11/Chance10Day.jpeg"),
+                                                    Image.network(
+                                                        "https://upload.wikimedia.org/wikipedia/en/1/11/Chance10Day.jpeg"),
                                                     Positioned(
                                                       bottom: 0.0,
                                                       left: 0.0,
@@ -161,17 +164,6 @@ class _SongList extends State<SongList> {
                                       //TODO in this container, show the latest 5 songs
                                     ),
                                   ),
-//                                  Container(
-//                                    child: Container(
-//                                    decoration: new BoxDecoration(
-//                                      color: Colors.purple,
-//                                      gradient: new LinearGradient(
-//                                        colors: [Colors.white, Colors.white.withOpacity(0.0), Colors.white.withOpacity(0.0), Colors.white],
-//                                        stops: [0, .05, .95, 1],
-//                                      ),
-//                                    ),
-//                                    ),
-//                                  ),
                                 ],
                               ),
                             ),
@@ -190,7 +182,9 @@ class _SongList extends State<SongList> {
                               ),
                             ),
                           ),
-                          Padding(padding: EdgeInsets.symmetric(vertical: 8.0),),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                          ),
                           Container(
                             width: MediaQuery.of(context).size.width,
                             color: Colors.transparent,
@@ -206,39 +200,44 @@ class _SongList extends State<SongList> {
                                       itemBuilder: (context, j) {
                                         Music current = getAtIndex(i)[j];
                                         return Row(
-                                            children: <Widget>[
-                                              Image.network(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: <Widget>[
+                                            ClipRRect(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(4.0)),
+                                              child: Image.network(
                                                 current.getAlbumImg,
                                                 width: 120,
                                                 height: 120,
                                               ),
-                                              Padding(
-                                                  padding: EdgeInsets.symmetric(
-                                                      horizontal: 8.0)),
-                                              Container(
-                                                height: 120,
-                                                child: Align(
-                                                  alignment: Alignment.topLeft,
-                                                  child: Column(
-                                                    children: <Widget>[
-                                                      Text(
-                                                        current.getTitle +
-                                                            "\n" +
-                                                            current.getArtist +
-                                                            "\n" +
-                                                            current.getAlbum,
-                                                        style: TextStyle(
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.w500,
-                                                        ),
+                                            ),
+                                            Padding(
+                                                padding: EdgeInsets.symmetric(
+                                                    horizontal: 8.0)),
+                                            Container(
+                                              child: Align(
+                                                alignment: Alignment.topLeft,
+                                                child: Column(
+                                                  children: <Widget>[
+                                                    Text(
+                                                      current.getTitle +
+                                                          "\n" +
+                                                          current.getArtist +
+                                                          "\n" +
+                                                          current.getAlbum,
+                                                      style: TextStyle(
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.w500,
                                                       ),
-                                                    ],
-                                                  ),
+                                                    ),
+                                                  ],
                                                 ),
-                                              )
-                                            ],
-                                          );
+                                              ),
+                                            )
+                                          ],
+                                        );
                                       })
                                 ],
                               ),
@@ -246,10 +245,23 @@ class _SongList extends State<SongList> {
                           ),
                           Padding(
                               padding: EdgeInsets.symmetric(
-                                  vertical: defaultPadding * 1.5)),
-                          //Other section
+                                  vertical: defaultPadding)),
+                          //Other section----------------------------------
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Other Selections",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                          ),
                           Container(
-                            height: 1040.0,
+                            //height: 1040.0,
                             width: MediaQuery.of(context).size.width,
                             color: Colors.transparent,
                             child: Container(
@@ -257,17 +269,53 @@ class _SongList extends State<SongList> {
                               decoration: defaultDecoration,
                               child: Column(
                                 children: <Widget>[
-                                  Align(
-                                    alignment: Alignment.topLeft,
-                                    child: Text(
-                                      "Other Selections",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 20,
-                                      ),
-                                    ),
+                                  ListView.builder(
+                                    shrinkWrap: true,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    itemCount: featured(getAtIndex(i)).length,
+                                    itemBuilder: (context, j) {
+                                      Music current = getAtIndex(i)[j];
+                                      return Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(4.0)),
+                                            child: Image.network(
+                                              current.getAlbumImg,
+                                              width: 90,
+                                              height: 90,
+                                            ),
+                                          ),
+                                          Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 8.0)),
+                                          Container(
+                                            child: Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Column(
+                                                children: <Widget>[
+                                                  Text(
+                                                    current.getTitle +
+                                                        "\n" +
+                                                        current.getArtist +
+                                                        "\n" +
+                                                        current.getAlbum,
+                                                    style: TextStyle(
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      );
+                                    },
                                   ),
-                                  //TODO add all other songs here
                                 ],
                               ),
                             ),
