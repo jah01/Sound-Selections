8280ceffff7818e9cb18d23b714f6a6c42ac17dd
Joseph Hilber <joeyhilber@gmail.com>
2020-07-22 11:16:16 -0400

    Upadte UI for details screen.

--- lib/data/music.dart
+++ lib/data/music.dart
@@ -50,6 +50,14 @@ class Music {
     return this.isFeatured;
   }
 
+  DateTime get getRelease {
+    return this.release;
+  }
+
+  String get getDesc {
+    return this.description;
+  }
+
 }
 
 List<Music> songs = [
@@ -60,7 +68,7 @@ List<Music> songs = [
     album: "10 Day",
     label: "N/A",
     release: DateTime(2012, 4, 3),
-    description: "N/A",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget. Consectetur a erat nam at lectus urna duis convallis convallis. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Sed risus pretium quam vulputate dignissim suspendisse in est. In fermentum et sollicitudin ac orci phasellus egestas. Amet nulla facilisi morbi tempus iaculis urna id volutpat lacus. Nunc sed id semper risus in hendrerit gravida rutrum quisque. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Arcu non sodales neque sodales ut. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Massa id neque aliquam vestibulum morbi blandit cursus risus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Rutrum quisque non tellus orci. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget. Consectetur a erat nam at lectus urna duis convallis convallis. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Sed risus pretium quam vulputate dignissim suspendisse in est. In fermentum et sollicitudin ac orci phasellus egestas. Amet nulla facilisi morbi tempus iaculis urna id volutpat lacus. Nunc sed id semper risus in hendrerit gravida rutrum quisque. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Arcu non sodales neque sodales ut. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Massa id neque aliquam vestibulum morbi blandit cursus risus. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Rutrum quisque non tellus orci. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet.",
     //artistImg: "https://upload.wikimedia.org/wikipedia/commons/0/03/Chance_the_Rapper_2018_February.png",
     artistImg: "chance.png",
     artistC: "FOX Sports: https://www.youtube.com/watch?v=YT86shZnD38, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=72191644",
--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,26 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:soundselections/defaults.dart';
 import '../../details/details_screen.dart';
 import '../../../data/music.dart';
+
+Widget _flightShuttleBuilder(
+    BuildContext flightContext,
+    Animation<double> animation,
+    HeroFlightDirection flightDirection,
+    BuildContext fromHeroContext,
+    BuildContext toHeroContext,
+    ) {
+  return DefaultTextStyle(
+    style: DefaultTextStyle.of(toHeroContext).style,
+    child: toHeroContext.widget,
+  );
+}
 
 class Body extends StatelessWidget {
   final Music song;
   final Key k;
+
   Body(this.song, this.k);
 
   //const Body({Key key, this.song}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-      return Container(
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            Hero(
-              tag: k,
+    return Hero(tag: k, flightShuttleBuilder: _flightShuttleBuilder, transitionOnUserGestures: true, child:Container(
+      color: Colors.white,
+      child: Stack(
+        children: <Widget>[
+      Container(
+            width: MediaQuery.of(context).size.width,
+//            foregroundDecoration: new BoxDecoration(
+//              gradient: new LinearGradient(
+//                begin: Alignment.topCenter,
+//                end: Alignment.bottomCenter,
+//                colors: [
+//                  Colors.white.withOpacity(0.0),
+//                  Colors.white,
+//                ],
+//                stops: [.5, .8],
+//              ),
+//            ),
+            child: FittedBox(
               child: Image.asset("assets/images/" + song.getAlbumImg),
+              fit: BoxFit.fill,
             ),
-            Text("testing"),
-          ],
-        ),
-      );
+          ),
+          SingleChildScrollView(child: Column(
+
+            children: <Widget>[
+//              Container(
+//                height: MediaQuery.of(context).size.width * .4,
+//              ),
+              Container(
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.white.withOpacity(0.0),
+                      Colors.white.withOpacity(0.0),
+                      Colors.white.withOpacity(0.5),
+                      Colors.white,
+                    ],
+                    stops: [0, .5, .7, .85],
+                  ),
+                ),
+                alignment: Alignment.centerLeft,
+                height: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(song.getTitle, style: TextStyle(fontSize: 24, color: Colors.grey[800], fontWeight: FontWeight.w600),),
+                    Wrap(
+                      children: <Widget>[
+                        Text("by ${song.getArtist}", style: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w600),),
+                        Container(padding: EdgeInsets.symmetric(horizontal: defaultPadding / 2), child: Text("•", style: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w600),),),
+                        Text("on ${song.getAlbum}", style: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w600),),
+                        Container(padding: EdgeInsets.symmetric(horizontal: defaultPadding / 2), child: Text("•", style: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w600),),),
+                        Text(song.getRelease.year.toString(), style: TextStyle(fontSize: 16, color: Colors.grey[600], fontWeight: FontWeight.w600),),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(defaultPadding),
+                alignment: Alignment.topLeft,
+                child: Text(song.getDesc, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.grey[400]),),
+              ),
+            ],
+          ),
+          ),
+        ],
+      ),
+    ),);
   }
 }
